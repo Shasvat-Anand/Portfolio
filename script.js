4edd0053486d29ce66d7580509ca4fc1688a180d
ANAND KR. AGRAHARI <163381623+Shasvat-Anand@users.noreply.github.com>
2025-04-21 02:09:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#element', {
+    strings: ['Web Developer', 'Video Editor', 'Graphic Designer', 'Content Creator.'],
+    typeSpeed: 90,
+});
+
+var typed = new Typed('#element1', {
+    strings: ['What I have doing so far.'],
+    typeSpeed: 90,
+});
